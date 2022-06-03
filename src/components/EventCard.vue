43c343ce734b489d6d896014286dbceb3ea9af57
Joaquin Nuñez <magnanimoysimple@gmail.com>
2022-06-03 18:48:29 -0300

    first commit

---
+++ src/components/EventCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="event-card">
+    <span>@ {{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  props: {
+    event: Object,
+  },
+};
+</script>
+<style scoped>
+.event-card {
+  margin: 10px;
+  width: 400px;
+  padding: 20px;
+  border: 1px solid #000;
+}
+.event-card:hover {
+  box-shadow: 10px 15px 40px #777;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="events">
+    <h1>Eventos</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 123456,
+          category: "animal Welfare",
+          title: "Cat adoption day",
+          description: "Find your new feline friend.",
+          location: "meow town",
+          date: "june 5, 2022",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "kat Laydee",
+        },
+        {
+          id: 1253456,
+          category: "animal Welfare",
+          title: "Cat adoption day",
+          description: "Find your new feline friend.",
+          location: "meow town",
+          date: "june 5, 2022",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "kat Laydee",
+        },
+        {
+          id: 1243456,
+          category: "animal Welfare",
+          title: "Cat adoption day",
+          description: "Find your new feline friend.",
+          location: "meow town",
+          date: "june 5, 2022",
+          time: "12:00",
+          petsAllowed: true,
+          organizer: "kat Laydee",
+        },
+      ],
+    };
   },
 };
 </script>
+
+<style scoped>
+.events {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-flow: column wrap;
+}
+</style>
